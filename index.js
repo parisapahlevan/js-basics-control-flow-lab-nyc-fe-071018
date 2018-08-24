0e0a1864c52af12fb5e10c5a84e700a34480ef26
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-24 22:27:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance){
+ if (distance <= 400) {
+  return "This one is on me!";
+   } else if (distance > 2000) { 
+    return "I will gladly take your thirty bucks.";
+   } else if (distance > 2500){
+     return "No can do.";
+}
+}
+
+
+function ternaryCheckCity(isCity){
+ return (isCity ? "Ok, sounds good." : "No go."); 
+}
+
+
+
+function switchOnCharmFromTip(tip) {
+  case 'generous':
+    console.log('Thank you so much.');
+    break;
+  case 'not as generous':
+    console.log('Thank you.');
+    break;
+  case 'not generous':
+    console.log('Bye.');
+}
+
+ 
